960ae67af0ebf2b4c3505b8926671afb33be7f03
Rango24 <125489323+Rango24@users.noreply.github.com>
2023-02-16 09:44:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import  streamlit
 streamlit.title('My Parents New Healthy Diner!')
 streamlit.header('Breakfast Menu')
-streamlit.tetx('Omega 3 & Blueberry Oatmeal')
-streamlit.tetx('Kale, Spinach & Rocket Smoothy')
-streamlit.tetx('Hard Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothy')
+streamlit.text('Hard Boiled Free-Range Egg')
 
 print('testing if normal  print works')
