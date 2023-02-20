2de53eb178d269966cc7a8982a38133be7d79fa1
Rango24 <125489323+Rango24@users.noreply.github.com>
2023-02-20 23:22:32 +0530

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,9 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
+
+#  
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_normalized)
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
